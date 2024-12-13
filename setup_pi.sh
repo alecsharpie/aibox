55a6c1610ad0ed3d91893a07bb78c2187712f708
Alec <alecsharpie@gmail.com>
2024-12-13 22:43:17 +1100

    portaudio

--- setup_pi.sh
+++ setup_pi.sh
@@ -1,5 +1,5 @@
 sudo apt-get install libasound-dev
-git clone https://github.com/alecsharpie/portaudio
+git clone https://github.com/PortAudio/portaudio.git
 cd portaudio
 ./configure && make
 sudo make install

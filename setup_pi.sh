498146863e893e09ae8edfbc4ca4a41bc01dd597
Alec <alecsharpie@gmail.com>
2024-12-13 23:35:46 +1100

    pyaudio deps

--- setup_pi.sh
+++ setup_pi.sh
@@ -2,3 +2,6 @@ sudo apt-get install libasound-dev
 sudo apt-get install python3-pyaudio
 
 sudo chmod +x /etc/rc.local
+sudo nano /etc/rc.local
+# add 'uv run python ~/aibox/main.py' before 'exit 0'
+# ctrl x, y, enter

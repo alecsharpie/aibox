7009cd110ca1997c4d1b5419293ba21eb0272e2e
Alec <alecsharpie@gmail.com>
2024-12-14 17:46:10 +1100

    setup

--- setup_pi.sh
+++ setup_pi.sh
@@ -1,7 +1,8 @@
 sudo apt-get install libasound-dev
+sudo apt-get install portaudio19-dev
 sudo apt-get install python3-pyaudio
 
-sudo chmod +x /etc/rc.local
-sudo nano /etc/rc.local
+# sudo chmod +x /etc/rc.local
+# sudo nano /etc/rc.local
 # add 'uv run python ~/aibox/main.py' before 'exit 0'
 # ctrl x, y, enter

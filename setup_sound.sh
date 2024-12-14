2b650d1d3ee6bb049264c69cf4ff5c2421b43fa9
Alec <alecsharpie@gmail.com>
2024-12-14 18:37:03 +1100

    user and global sounds setup

--- setup_sound.sh
+++ setup_sound.sh
@@ -1,20 +1,4 @@
-echo 'pcm.!default {
-    type asym
-    playback.pcm {
-        type plug
-        slave.pcm "hw:3,0"
-    }
-    capture.pcm {
-        type plug
-        slave.pcm "hw:3,0"
-    }
-}
-
-ctl.!default {
-    type hw
-    card 3
-}' > ~/.asoundrc
-
+# Create user-specific config
 echo 'pcm.!default {
     type asym
     playback.pcm {
@@ -24,6 +8,68 @@ echo 'pcm.!default {
     capture.pcm {
         type plug
         slave.pcm "hw:3,0"  # ATR4697-USB
+    }
+}
+
+defaults.pcm.rate_converter "samplerate_best"
+
+pcm.microphone {
+    type plug
+    slave {
+        pcm "hw:3,0"  # ATR4697-USB
+        format S16_LE
+        rate 44100
+        channels 1
+    }
+}
+
+pcm.speaker {
+    type plug
+    slave {
+        pcm "hw:2,0"  # bcm2835 Headphones
+        format S16_LE
+        rate 48000
+        channels 2
+    }
+}
+
+ctl.!default {
+    type hw
+    card 2  # bcm2835 Headphones
+}' > ~/.asoundrc
+
+# Create system-wide config
+echo 'pcm.!default {
+    type asym
+    playback.pcm {
+        type plug
+        slave.pcm "hw:2,0"  # bcm2835 Headphones
+    }
+    capture.pcm {
+        type plug
+        slave.pcm "hw:3,0"  # ATR4697-USB
+    }
+}
+
+defaults.pcm.rate_converter "samplerate_best"
+
+pcm.microphone {
+    type plug
+    slave {
+        pcm "hw:3,0"  # ATR4697-USB
+        format S16_LE
+        rate 44100
+        channels 1
+    }
+}
+
+pcm.speaker {
+    type plug
+    slave {
+        pcm "hw:2,0"  # bcm2835 Headphones
+        format S16_LE
+        rate 48000
+        channels 2
     }
 }
 
